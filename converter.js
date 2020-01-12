d5a200692f7cb555552a46127b883d8c997e534f
SetWeather <49021599+SetWeather@users.noreply.github.com>
2020-01-12 13:00:26 +0200

    Update converter.js

--- converter.js
+++ converter.js
@@ -2,46 +2,123 @@ const selectInput = document.querySelector("#select-input")
 const imageFieldChoice = document.querySelector("#image-field-choice")
 
 selectInput.addEventListener("change", function informFile(input) {
-	const imgs = [...this.files]
+	const imgsArray = [...this.files]
+	let imgNumber = 0
+	imgsArray.forEach(img => {
 
-	imgs.map(img => {
-		console.log(img.name)
-		console.log(img.type)
-		console.log(img.size)
+		const imgBlock = document.createElement("div")
+		imgBlock.classList.add("img-block")
 
-		function createBlock () {
-			const imgBlock = document.createElement("div")
-			imgBlock.classList.add("img-block")
+		const canvas = document.createElement("canvas")
+		const context = canvas.getContext("2d")
+		canvas.classList.add("img-load-block")
+		canvas.id = "imgCanvas"
 
-			const imgLoadBlock = document.createElement("div")
-			imgLoadBlock.classList.add("img-load-block")
+		const imgElement = document.createElement("img")
+		imgElement.classList.add("img-load")
+		imgElement.id = "img-load"
 
-			const imgElement = document.createElement("img")
-			imgElement.classList.add("img-load")
-			imgElement.id = "img-load"
+		const imgName = document.createElement("h3")
+		imgName.classList.add("img-name")
+		imgName.textContent = img.name
 
-			const imgLoad = document.querySelector("#img-load")
-			imgElement.src = readURL()
+		const reader = new FileReader()
 
-			const imgName = document.createElement("h3")
-			imgName.classList.add("img-name")
-			imgName.textContent = img.name
+		reader.readAsDataURL(imgsArray[imgNumber])
 
-			imageFieldChoice.appendChild(imgBlock)
-			imgBlock.appendChild(imgLoadBlock)
-			imgLoadBlock.appendChild(imgElement)
-			imgBlock.appendChild(imgName)
+		reader.addEventListener("load", function getImgUrl(img) {
+			imgElement.src = img.target.result
+			let imgUrl = imgElement.src
+		})
 
-			function readURL(img) {
-				const reader = new FileReader()
+		imgElement.addEventListener("load", (img) => {
+			if (imgsArray.indexOf(imgElement) != undefined) {
+				var imgWidth = imgElement.naturalWidth
+				var imgHeight = imgElement.naturalHeight
 
-				reader.readAsDataURL(imgs[0])
+				canvas.width = imgWidth
+				canvas.height = imgHeight
+				context.drawImage(imgElement, 0, 0, imgWidth, imgHeight)
 
-				reader.addEventListener("load", (img) => {
-					imgElement.src = img.target.result
-				})
+				getImgData(img, imgWidth, imgHeight)
+			} else {
+				imgName.textContent = "An error occurred while loading the image"
 			}
+		})
+
+		imageFieldChoice.appendChild(imgBlock)
+		imgBlock.appendChild(canvas)
+		canvas.appendChild(imgElement)
+		imgBlock.appendChild(imgName)
+
+		function getImgData (img, imgWidth, imgHeight) {
+			const allPixelData = []
+			const pixelData = {
+				red		: imgData.data[i]
+				green	: imgData.data[i+1]
+				blue	: imgData.data[i+2]
+				alpha	: imgData.data[i+3]
+			}
+			let imgData = context.getImageData(imgElement, 0, 0, 200, 200)
+			for (let i = 0; i < allPixelData.length; i += 4) {
+				let red = imgData.data[i]
+				let green = imgData.data[i+1]
+				let blue = imgData.data[i+2]
+				let alpha = imgData.data[i+3]
+				pixelData.push(red, green, blue, alpha)
+			}
+			allPixelData.push(pixelData)
 		}
-		createBlock ()
+		return imgNumber++
 	})
 })
+
+
+
+// const selectInput = document.querySelector("#select-input")
+// const imageFieldChoice = document.querySelector("#image-field-choice")
+
+// selectInput.addEventListener("change", function informFile(input) {
+// 	const imgsArray = [...this.files]
+
+// 	imgs.map(img => {
+// 		console.log(img.name)
+// 		console.log(img.type)
+// 		console.log(img.size)
+
+// 		function createBlock () {
+// 			const imgBlock = document.createElement("div")
+// 			imgBlock.classList.add("img-block")
+
+// 			const imgLoadBlock = document.createElement("div")
+// 			imgLoadBlock.classList.add("img-load-block")
+
+// 			const imgElement = document.createElement("img")
+// 			imgElement.classList.add("img-load")
+// 			imgElement.id = "img-load"
+
+// 			const imgLoad = document.querySelector("#img-load")
+// 			imgElement.src = readURL()
+
+// 			const imgName = document.createElement("h3")
+// 			imgName.classList.add("img-name")
+// 			imgName.textContent = img.name
+
+// 			imageFieldChoice.appendChild(imgBlock)
+// 			imgBlock.appendChild(imgLoadBlock)
+// 			imgLoadBlock.appendChild(imgElement)
+// 			imgBlock.appendChild(imgName)
+
+// 			function readURL(img) {
+// 				const reader = new FileReader()
+
+// 				reader.readAsDataURL(imgs[0])
+
+// 				reader.addEventListener("load", (img) => {
+// 					imgElement.src = img.target.result
+// 				})
+// 			}
+// 		}
+// 		createBlock ()
+// 	})
+// })
