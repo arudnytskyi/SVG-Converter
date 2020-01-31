c319b47c40edb52ae762da2301052c71f3c24a46
SetWeather <49021599+SetWeather@users.noreply.github.com>
2020-02-01 00:00:50 +0200

    Update converter.js

--- converter.js
+++ converter.js
@@ -31,43 +31,78 @@ selectInput.addEventListener("change", function informFile(input) {
 			let imgUrl = imgElement.src
 		})
 
-		imgElement.addEventListener("load", (img) => {
-			if (imgsArray.indexOf(imgElement) != undefined) {
-				var imgWidth = imgElement.naturalWidth
-				var imgHeight = imgElement.naturalHeight
-
-				canvas.width = imgWidth
-				canvas.height = imgHeight
-				context.drawImage(imgElement, 0, 0, imgWidth, imgHeight)
-
-				getImgData(img, imgWidth, imgHeight)
-			} else {
-				imgName.textContent = "An error occurred while loading the image"
-			}
-		})
-
 		imageFieldChoice.appendChild(imgBlock)
 		imgBlock.appendChild(canvas)
 		canvas.appendChild(imgElement)
 		imgBlock.appendChild(imgName)
 
-		function getImgData (img, imgWidth, imgHeight) {
-			const allPixelData = []
-			const pixelData = {
-				red		: imgData.data[i]
-				green	: imgData.data[i+1]
-				blue	: imgData.data[i+2]
-				alpha	: imgData.data[i+3]
+		imgElement.addEventListener("load", (img) => {
+			if (imgsArray.indexOf(imgElement) != undefined) {
+				let imgWidth = imgElement.naturalWidth
+				let imgHeight = imgElement.naturalHeight
+
+				canvas.width = imgWidth
+				canvas.height = imgHeight
+				context.drawImage(imgElement, 0, 0, imgWidth, imgHeight)
+
+				let imgData = context.getImageData(0, 0, imgWidth, imgHeight)
+				console.log(imgData.data)
+				getPixelData (imgData)
+
+			} else {
+				imgName.textContent = "An error occurred while loading the image"
 			}
-			let imgData = context.getImageData(imgElement, 0, 0, 200, 200)
-			for (let i = 0; i < allPixelData.length; i += 4) {
-				let red = imgData.data[i]
-				let green = imgData.data[i+1]
-				let blue = imgData.data[i+2]
-				let alpha = imgData.data[i+3]
-				pixelData.push(red, green, blue, alpha)
+		})
+
+		function getPixelData (imgData, imgWidth, imgHeight) {
+			const allPixel = []
+			for (let i = 0; i < imgData.data.length; i+=4) {
+				const pixelData = {
+					red		: +imgData.data[i],
+					green	: +imgData.data[i+1],
+					blue	: +imgData.data[i+2],
+					alpha	: imgData.data[i+3] * 0.00392156862745098
+				}
+				allPixel.push(pixelData)
 			}
-			allPixelData.push(pixelData)
+			console.log(allPixel.length)
+			return createSvgImg(imgData, imgWidth, imgHeight, allPixel)
+		}
+
+		function createSvgImg (imgData, imgWidth, imgHeight, allPixel) {
+			const svgArray = []
+			const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+			let rectSize = canvas.width / allPixel.length
+			let rectSVGX = 0
+			let rectSVGY = 0
+			svg.setAttribute("width", canvas.width)
+			svg.setAttribute("height", canvas.height)
+			svg.setAttribute("viewBox", `0, 0, ${canvas.width}, ${canvas.height}`)
+
+			imageFieldChoice.appendChild(svg)
+
+			for(let i = 0; i < allPixel.length; i++) {
+				const rectSVG = document.createElement("rect")
+				rectSVG.id = "square"
+				let rectLength = document.querySelectorAll("#square").length - 2
+				rectSVG.setAttribute("fill", `rgba(${allPixel[i].red}, ${allPixel[i].green}, ${allPixel[i].blue}, ${allPixel[i].alpha})`)
+				rectSVG.setAttribute("width", 1)
+				rectSVG.setAttribute("height", 1)
+				rectSVG.setAttribute("x", rectSVGX)
+				rectSVG.setAttribute("y", rectSVGY)
+				if (rectSVGX < canvas.width - 1) 
+				{rectSVGX = rectSVGX + 1}
+				else {
+					rectSVGX = 0 
+					rectSVGY = rectSVGY + 1
+				}
+				svgArray.push(rectSVG)
+				svg.appendChild(rectSVG)
+				if (rectSVG.getAttribute("fill") == `rgba(0, 0, 0, ${allPixel[i].alpha})`) 
+					{svg.removeChild(rectSVG)}
+			console.log(svgArray)
+			console.log(svg)
+			}
 		}
 		return imgNumber++
 	})
